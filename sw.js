76c50d40c718e10620ae09975e9c4c3ae37879be
qwersteve07 <qwersteve07@gmail.com>
2023-05-27 18:50:59 +0800

    feat: impl sw with cache

---
+++ sw.js
@@ -0,0 +1,44 @@
+// add resources to cache
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open("v1");
+  await cache.addAll(resources);
+};
+
+// service worker install event
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    addResourcesToCache(["/", "/src/assets/react.svg", "/vite.svg"])
+  );
+});
+
+// put key/value pair in cache
+const putInCache = async (request, response) => {
+  const cache = await caches.open("v1");
+  await cache.put(request, response);
+};
+
+const cacheFirst = async (request) => {
+  // try to fetch from caches
+  const responseFromCache = await caches.match(request);
+
+  if (responseFromCache) {
+    console.log("request from cache");
+    console.log(responseFromCache);
+    return responseFromCache;
+  }
+
+  // try to fetch from network
+  try {
+    const responseFromNetwork = await fetch(request);
+    await putInCache(request, responseFromNetwork);
+    return responseFromNetwork;
+  } catch (error) {
+    console.log(error);
+    return new Response("Network Error");
+  }
+};
+
+// service worker fetch event
+self.addEventListener("fetch", (event) => {
+  event.respondWith(cacheFirst(event.request));
+});
