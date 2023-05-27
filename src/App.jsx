d992ede24b837953c45b989a3143ec9566726c7a
qwersteve07 <qwersteve07@gmail.com>
2023-05-27 20:53:03 +0800

    feat: impl wakeLock

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
   return (
     <>
       <div>
+        <WakeUpToggle />
         <Cat />
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -32,6 +33,53 @@ function App() {
       </p>
       <InstallPromptButton />
     </>
+  );
+}
+
+function WakeUpToggle() {
+  const [show, setShow] = useState(false);
+  const [status, setStatus] = useState("activate"); // [active, inactive, undefined
+  const wakeLock = useRef(null);
+
+  const toggleWakeLock = async () => {
+    if (wakeLock.current === null) {
+      //  wakeLock is not active
+      try {
+        wakeLock.current = await navigator.wakeLock.request("screen");
+        setStatus("deactivate");
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      //  wakeLock is active
+      try {
+        await wakeLock.current.release();
+        wakeLock.current = null;
+        setStatus("activate");
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if ("wakeLock" in navigator) {
+      setShow(true);
+    }
+  }, []);
+
+  return (
+    <button
+      style={{
+        display: show ? "block" : "none",
+        position: "fixed",
+        left: "30px",
+        top: "90px",
+      }}
+      onClick={toggleWakeLock}
+    >
+      {status} WakeLock
+    </button>
   );
 }
 
