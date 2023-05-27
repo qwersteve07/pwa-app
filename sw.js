07a1fbf90c5febbdc8b9f33cbf6fbc078e70536b
qwersteve07 <qwersteve07@gmail.com>
2023-05-28 00:45:58 +0800

    feat: impl offline mechanism

--- sw.js
+++ sw.js
@@ -6,43 +6,18 @@ const addResourcesToCache = async (resources) => {
 
 // service worker install event
 self.addEventListener("install", (event) => {
+  // make sure the service worker is activated immediately
   self.skipWaiting();
 
   event.waitUntil(
-    addResourcesToCache(["/", "/src/assets/react.svg", "/vite.svg"])
+    addResourcesToCache([
+      "/",
+      "/src/assets/react.svg",
+      "/vite.svg",
+      // using {cache: reload} to make sure fetch file from network
+      new Request("./offline.html", { cache: "reload" }),
+    ])
   );
-});
-
-// put key/value pair in cache
-const putInCache = async (request, response) => {
-  const cache = await caches.open("v1");
-  await cache.put(request, response);
-};
-
-const cacheFirst = async (request) => {
-  // try to fetch from caches
-  const responseFromCache = await caches.match(request);
-
-  if (responseFromCache) {
-    console.log("request from cache");
-    console.log(responseFromCache);
-    return responseFromCache;
-  }
-
-  // try to fetch from network
-  try {
-    const responseFromNetwork = await fetch(request);
-    await putInCache(request, responseFromNetwork);
-    return responseFromNetwork;
-  } catch (error) {
-    console.log(error);
-    return new Response("Network Error");
-  }
-};
-
-// service worker fetch event
-self.addEventListener("fetch", (event) => {
-  event.respondWith(cacheFirst(event.request));
 });
 
 const enableNavigationPreload = async () => {
@@ -53,6 +28,43 @@ const enableNavigationPreload = async () => {
 
 // service worker activate event
 self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
+  // tell the active sw to take control of the page immediately
+  self.clients.claim();
+  // enabled navigation preload
   event.waitUntil(enableNavigationPreload());
+});
+
+// put key/value pair in cache
+const putInCache = async (request, response) => {
+  const cache = await caches.open("v1");
+  await cache.put(request, response);
+};
+
+const cacheFirst = async (event) => {
+  // try to fetch from caches
+  const cacheResponse = await caches.match(event.request);
+  if (cacheResponse) return cacheResponse;
+
+  // try to fetch from preload
+  const preloadResponse = await event.preloadResponse;
+  if (preloadResponse) return preloadResponse;
+
+  // try to fetch from network
+  try {
+    const networkResponse = await fetch(event.request);
+    // await putInCache(event.request, networkResponse);
+    return networkResponse;
+  } catch (error) {
+    if (event.request.mode === "navigate") {
+      const offlineCacheResponse = await caches.match("./offline.html");
+      return offlineCacheResponse;
+    } else {
+      return new Response("Network Error");
+    }
+  }
+};
+
+// service worker fetch event
+self.addEventListener("fetch", (event) => {
+  event.respondWith(cacheFirst(event));
 });
