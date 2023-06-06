465ccb592e0be1b362e3342f8a3ee8f342901cc5
SteveLee <qwersteve07@gmail.com>
2023-06-06 11:28:02 +0800

    tes: remove camera

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import BatteryStatus from "./features/battery-status/battery-status";
 import InstallPromptButton from "./features/install-prompt-button/install-prompt-button";
 import WakeUpToggler from "./features/wake-lock-toggler/wake-lock-toggler";
 import MediaSession from "./features/media-session/media-session";
-import Camera from "./features/camera/camera";
+// import Camera from "./features/camera/camera";
 import PushNotification from "./features/push-notification/push-notification";
 
 function App() {
@@ -20,7 +20,7 @@ function App() {
   return (
     <>
       <div>
-        <Camera />
+        {/* <Camera /> */}
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
